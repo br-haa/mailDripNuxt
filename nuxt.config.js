7d0eddb69724dd6e51996df4c5186ec08137532d
haa-jp <Jasper27>
2021-04-07 10:41:49 -0400

    minor changes to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   env: { strapiBaseUri },
   head: {
-    title: 'mailDripNuxt',
+  title: 'mailDripNuxt',
     htmlAttrs: {
       lang: 'en',
     },
