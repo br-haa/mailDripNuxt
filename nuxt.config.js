0b1e57ad93fd38caf7266a868d29655497860557
Benjamin <work@benjaminrussell.me>
2021-04-08 09:59:46 -0500

    fix: maybe?

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-const strapiBaseUri = process.env.API_URL || 'http://localhost:1337'
+const strapiBaseUri = process.env.API_URL || 'https://strapi-6ge4.onrender.com'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
